f72482e6befddac2ae94d93586918c3cf6c97dbc
bgeffrault <bgeffrault@outlook.fr>
2022-11-27 11:57:59 +0100

    refactor: improve code

--- src/hooks/useToggleRightSidebar.ts
+++ src/hooks/useToggleRightSidebar.ts
@@ -1,94 +1,101 @@
-import { useCallback, useEffect, useRef } from "react";
-import gsap from "gsap";
+import { useCallback, useEffect, useMemo, useRef } from 'react';
+import gsap from 'gsap';
 
-type useToggleRightSidebarProps = {
-  initialState?: "opened" | "closed";
-  sidebarWidth?: number | string;
+type UseToggleRightSidebarProps = {
+  initialState?: 'opened' | 'closed';
+  sidebarWidth?: number;
   mobileOnly?: boolean;
   isSmallScreen?: boolean;
 };
 
 export const useToggleRightSidebar = ({
-  initialState = "opened",
+  initialState = 'opened',
   sidebarWidth = 247,
   mobileOnly,
-  isSmallScreen = false
-}: useToggleRightSidebarProps) => {
-  const bodyRef = useRef<HTMLDivElement | null>(null);  
-  const sidebarRef = useRef<HTMLDivElement | null>(null);  
-  const animationState = useRef<{ tl: GSAPTimeline | null; lastState: string }>(
-    {
-      lastState: isSmallScreen ? "closed" : initialState,
-      tl: null,
-    }
+  isSmallScreen = false,
+}: UseToggleRightSidebarProps) => {
+  const bodyRef = useRef<HTMLDivElement | null>(null);
+  const sidebarRef = useRef<HTMLDivElement | null>(null);
+  const animationState = useRef<{ tl: GSAPTimeline | null; lastState: string }>({
+    lastState: isSmallScreen ? 'closed' : initialState,
+    tl: null,
+  });
+  const EASING = useMemo(
+    () => (isSmallScreen ? 'power2.inOut' : 'back.out(1)'),
+    [isSmallScreen],
   );
-  const EASING = isSmallScreen ? "power2.inOut" : "back.out(1)";
-  const DURATION = isSmallScreen ? 0.6 : 0.8;
+  const DURATION = useMemo(() => (isSmallScreen ? 0.6 : 0.8), [isSmallScreen]);
 
   const handleToggleSidebar = useCallback(
     async (callback?: () => any) => {
-      if (mobileOnly && !isSmallScreen) return;
-      if (animationState.current.lastState === "opened") {
-        animationState.current.lastState = "closed";
-        animationState.current.tl?.kill();
-        animationState.current.tl = gsap
-          .timeline({
-            paused: true,
-            defaults: { duration: DURATION, ease: EASING },
-          })
-          .to(sidebarRef.current, { xPercent: 100 }, 0);
-        if (isSmallScreen) {
-          animationState.current.tl.to(bodyRef.current, { xPercent: 0 }, 0);
-        } else {
-          animationState.current.tl.to(bodyRef.current, { width: "100%" }, 0);
-        }
-      } else {
-        animationState.current.lastState = "opened";
-        animationState.current.tl?.kill();
-        animationState.current.tl = gsap
-          .timeline({
-            paused: true,
-            defaults: { duration: DURATION, ease: EASING },
-          })
-          .fromTo(sidebarRef.current, { xPercent: 100 }, { xPercent: 0 }, 0);
-        if (isSmallScreen) {
-          animationState.current.tl.to(bodyRef.current, { xPercent: -100 }, 0);
-        } else {
-          animationState.current.tl.to(
-            bodyRef.current,
-            { width: `calc(100% - ${sidebarWidth})` },
-            0
-          );
-        }
-      }
-      if (callback && typeof callback === "function") {
+      if ((mobileOnly && !isSmallScreen) || !bodyRef.current || !sidebarRef.current)
+        return;
+
+      animationState.current.tl?.kill();
+      let tl = gsap.timeline({
+        paused: true,
+        defaults: { duration: DURATION, ease: EASING },
+      });
+      animationState.current.lastState === 'opened' ? closeSidebar() : openSidebar();
+
+      animationState.current.tl = tl;
+      if (typeof callback === 'function') {
         animationState.current.tl.call(callback);
       }
       await animationState.current.tl.restart();
+
+      function openSidebar() {
+        animationState.current.lastState = 'opened';
+        tl = tl.fromTo(sidebarRef.current, { xPercent: 100 }, { xPercent: 0 }, 0);
+        tl = isSmallScreen
+          ? tl.to(bodyRef.current, { xPercent: -100 }, 0)
+          : tl.to(
+              bodyRef.current,
+              {
+                width:
+                  (bodyRef.current as HTMLDivElement).offsetWidth - sidebarWidth,
+              },
+              0,
+            );
+      }
+
+      function closeSidebar() {
+        animationState.current.lastState = 'closed';
+        animationState.current.tl = tl.to(sidebarRef.current, { xPercent: 100 }, 0);
+        isSmallScreen
+          ? tl.to(bodyRef.current, { xPercent: 0 }, 0)
+          : tl.to(bodyRef.current, { width: '100%' }, 0);
+      }
     },
-    [isSmallScreen]
+    [DURATION, EASING, isSmallScreen, mobileOnly, sidebarWidth],
   );
 
   useEffect(() => {
-    if (animationState.current.lastState === "closed") {
+    if (animationState.current.lastState === 'closed') {
       gsap.set(sidebarRef.current, { xPercent: 100 });
-      gsap.set(bodyRef.current, { width: "100%" });
+      gsap.set(bodyRef.current, { width: '100%' });
+      return;
     }
-    if (animationState.current.lastState === "opened") {
-      if (isSmallScreen) {
-        gsap.set(bodyRef.current, { clearProps: "width" });
-        gsap.set(bodyRef.current, { xPercent: -100 });
-      }
-      if (!isSmallScreen) {
-        gsap.set(bodyRef.current, { clearProps: "xPercent" });
-        gsap.set(bodyRef.current, { width: `calc(100% - ${sidebarWidth})` });
-      }
-    }
-    if (mobileOnly && !isSmallScreen) {
-      gsap.set(sidebarRef.current, { clearProps: "xPercent" });
-      gsap.set(bodyRef.current, { clearProps: "xPercent" });
-    }
-  }, [isSmallScreen]);
 
-  return {handleToggleSidebar, bodyRef, sidebarRef};
+    if (isSmallScreen) {
+      gsap.set(bodyRef.current, { clearProps: 'width' });
+      gsap.set(bodyRef.current, { xPercent: -100 });
+      return;
+    }
+
+    if (!bodyRef.current) return;
+    gsap.set(bodyRef.current, { clearProps: 'xPercent' });
+    gsap.set(bodyRef.current, {
+      width: bodyRef.current.offsetWidth - sidebarWidth,
+    });
+  }, [isSmallScreen, mobileOnly, sidebarWidth]);
+
+  useEffect(() => {
+    if (mobileOnly && !isSmallScreen) {
+      gsap.set(sidebarRef.current, { clearProps: 'xPercent' });
+      gsap.set(bodyRef.current, { clearProps: 'xPercent' });
+    }
+  }, []);
+
+  return { handleToggleSidebar, bodyRef, sidebarRef };
 };
